17f6b1e1a9f08b68349123c608dadb135d486dae
William J. C. Nesbitt <wjcn@mac.com>
2020-08-08 18:17:29 -0700

    Revert "Revert "Builds for tvOS and watchOS""
    This reverts commit 7cde3691d469343cc4742a2be4da910f5bbb308d.

--- Sources/MySwiftLibrary/CIFilterExtension.swift
+++ Sources/MySwiftLibrary/CIFilterExtension.swift
@@ -4,8 +4,10 @@
 //  Created by William J. C. Nesbitt.
 //
 
+#if !os(watchOS)
 import CoreImage.CIFilter
 
 public extension CIFilter {
     @inlinable var localisedName: String { Self.localizedName(forFilterName: name)! }
 }
+#endif
