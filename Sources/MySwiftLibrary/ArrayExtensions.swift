eb1a624e6aac22cdc6ef961be7326ad414909b97
William J. C. Nesbitt <wjcn@mac.com>
2020-08-12 15:57:17 -0700

    Added Chunked Function to Array Extension.

--- Sources/MySwiftLibrary/ArrayExtensions.swift
+++ Sources/MySwiftLibrary/ArrayExtensions.swift
@@ -9,6 +9,16 @@ import Foundation.NSArray
 public extension Array {
     @inlinable var notEmpty: Bool { !isEmpty }
 
+    // How to split an array into chunks.
+    // https://www.hackingwithswift.com/example-code/language/how-to-split-an-array-into-chunks
+
+    func chunked(into size: Index) -> [[Element]] {
+        stride(from: startIndex, to: endIndex, by: size).map {
+            index in
+            Self(self[index ..< Swift.min(index + size, endIndex)])
+        }
+    }
+
     @inlinable static func + (leftSide: Self, rightSide: Element) -> Self {
         var result = leftSide
         result.append(rightSide)
