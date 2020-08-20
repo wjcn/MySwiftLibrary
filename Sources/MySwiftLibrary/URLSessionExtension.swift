1b2c943439682c1d0b84ba48fe51fd854e8a7262
William J. C. Nesbitt <wjcn@mac.com>
2020-08-20 02:47:11 -0700

    Fixed Return Type in URL Session Data Task. Added JSON Support to URL Session Data Task.

--- Sources/MySwiftLibrary/URLSessionExtension.swift
+++ Sources/MySwiftLibrary/URLSessionExtension.swift
@@ -7,12 +7,10 @@
 import Foundation.NSURLSession
 
 extension URLSession {
-    public typealias ResultBasedCompletionHandler = (Result<(Data, URLResponse?), Error>) -> Void
-
     @inlinable public func dataTask(with url: URL,
                                     queue: DispatchQueue = .main,
                                     with parameters: DispatchParameters3 = .defaults,
-                                    completionHandler: @escaping ResultBasedCompletionHandler) {
+                                    completionHandler: @escaping (Result<(Data, URLResponse?), Error>) -> Void) -> URLSessionDataTask {
         dataTask(with: url, completionHandler: {
             (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data, error == nil {
@@ -34,7 +32,7 @@ extension URLSession {
     @inlinable public func dataTask(with request: URLRequest,
                                     queue: DispatchQueue = .main,
                                     with parameters: DispatchParameters3 = .defaults,
-                                    completionHandler: @escaping ResultBasedCompletionHandler) {
+                                    completionHandler: @escaping (Result<(Data, URLResponse?), Error>) -> Void) -> URLSessionDataTask {
         dataTask(with: request, completionHandler: {
             (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data, error == nil {
@@ -50,6 +48,60 @@ extension URLSession {
                 return
             }
             fatalError() // This should not happen.
+        })
+    }
+
+    @inlinable public func jsonTask<T>(with url: URL,
+                                       decoder: JSONDecoder = JSONDecoder(),
+                                       queue: DispatchQueue = .main,
+                                       with parameters: DispatchParameters3 = .defaults,
+                                       completionHandler: @escaping (Result<(T, URLResponse?), Error>) -> Void) -> URLSessionDataTask where T: Decodable {
+        dataTask(with: url, queue: queue, with: parameters, completionHandler: {
+            (result: Result<(Data, URLResponse?), Error>) in
+            switch result {
+                case .success(let (data, response)):
+                    do {
+                        let decoded = try decoder.decode(T.self, from: data)
+                        queue.async(with: parameters, execute: {
+                            completionHandler(.success((decoded, response)))
+                        })
+                    } catch {
+                        queue.async(with: parameters, execute: {
+                            completionHandler(.failure(error))
+                        })
+                    }
+                case .failure(let error):
+                    queue.async(with: parameters, execute: {
+                        completionHandler(.failure(error))
+                    })
+            }
+        })
+    }
+
+    @inlinable public func jsonTask<T>(with request: URLRequest,
+                                       decoder: JSONDecoder = JSONDecoder(),
+                                       queue: DispatchQueue = .main,
+                                       with parameters: DispatchParameters3 = .defaults,
+                                       completionHandler: @escaping (Result<(T, URLResponse?), Error>) -> Void) -> URLSessionDataTask where T: Decodable {
+        dataTask(with: request, queue: queue, with: parameters, completionHandler: {
+            (result: Result<(Data, URLResponse?), Error>) in
+            switch result {
+                case .success(let (data, response)):
+                    do {
+                        let decoded = try decoder.decode(T.self, from: data)
+                        queue.async(with: parameters, execute: {
+                            completionHandler(.success((decoded, response)))
+                        })
+                    } catch {
+                        queue.async(with: parameters, execute: {
+                            completionHandler(.failure(error))
+                        })
+                    }
+                case .failure(let error):
+                    queue.async(with: parameters, execute: {
+                        completionHandler(.failure(error))
+                    })
+            }
         })
     }
 }
