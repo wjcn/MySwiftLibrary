b425c74a30a53e72ecfd3409c0d687aa2ea804df
William J. C. Nesbitt <wjcn@mac.com>
2020-09-20 14:30:45 -0700

    Rewrote Format Numbers.

--- Sources/MySwiftLibrary/FormatNumbers.swift
+++ Sources/MySwiftLibrary/FormatNumbers.swift
@@ -7,14 +7,98 @@
 
 import Foundation.NSNumberFormatter
 
-extension BinaryInteger {
+extension NSNumber {
     @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
-        NumberFormatter.localizedString(from: NSNumber(value: Int(self)), number: style)
+        NumberFormatter.localizedString(from: self, number: style)
+    }
+
+    @inlinable public func `as`(_ numberStyle: NumberFormatter.Style,
+                                _ maximumFractionalDigits: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = maximumFractionalDigits
+        numberFormatter.numberStyle = numberStyle
+        return numberFormatter.string(from: self)!
     }
 }
 
-extension BinaryFloatingPoint {
+extension Int {
     @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
-        NumberFormatter.localizedString(from: NSNumber(value: Double(self)), number: style)
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension Int8 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension Int16 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension Int32 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension Int64 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension UInt {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension UInt8 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension UInt16 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension UInt32 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension UInt64 {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+}
+
+extension Double {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+
+    @inlinable public func `as`(_ style: NumberFormatter.Style,
+                                _ maximumFractionalDigits: Int) -> String {
+        NSNumber(value: self).as(style, maximumFractionalDigits)
+    }
+}
+
+extension Float {
+    @inlinable public func `as`(_ style: NumberFormatter.Style) -> String {
+        NSNumber(value: self).as(style)
+    }
+
+    @inlinable public func `as`(_ style: NumberFormatter.Style,
+                                _ maximumFractionalDigits: Int) -> String {
+        NSNumber(value: self).as(style, maximumFractionalDigits)
     }
 }
